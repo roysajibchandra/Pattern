bf42dd4050b24aa81f0198332da4e0cf839b59c6
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-05-19 14:09:54 +0600

    Solid_Rhombus_pattern.java

---
+++ Solid_Rhombus_pattern.java
@@ -0,0 +1,39 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void solid_Rhombus_pattern(int N)
+	{
+		// 1st half
+		for(int i = 1; i<=N; i++)
+		{
+			// spaces
+			for(int j=1; j<=N-i; j++)
+			{
+				System.out.print("   ");
+			}
+			for(int j=1; j<=N; j++)
+			{
+				System.out.print("*  ");
+			}
+			System.out.println();
+			
+		}
+
+	}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner timtim = new Scanner(System.in);
+		
+		int N = timtim.nextInt();
+		// int Totalcols = timtim.nextInt();
+		solid_Rhombus_pattern(N);
+		
+	}
+}
